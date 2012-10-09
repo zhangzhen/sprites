2d0db27f7b7da9eb68492b2404c355d8ba989e3b
Zhen Zhang <zhangz.cs@gmail.com>
2012-10-09 11:18:20 +0800

    filter alignments with a given number of N

--- filter_out_reads.py
+++ filter_out_reads.py
@@ -2,14 +2,14 @@
 import sys
 import ParseFastQ
 
-def filterReads(inFilename, outFilename):
+def filterReads(inFilename, outFilename, numN=5):
     parser = ParseFastQ.ParseFastQ(inFilename)  # optional arg: headerSymbols allows changing the header symbols
     output = open(outFilename, 'w')
     total = 0
     rest = 0
     for record in parser:
         total += 1
-        if 'N' in record[1]:
+        if 'N'*numN in record[1]:
             continue
         output.write("%s\n%s\n%s\n%s\n" % (record[0], record[1], record[2], record[3]))
         rest += 1
