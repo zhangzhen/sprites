e06a431394a3fccee9e3153f24b677281cc65a55
Zhen Zhang <zhangz.cs@gmail.com>
2012-11-12 16:21:31 +0800

    separate out the tool filtering out single-end and paired-end reads

--- filter_out_reads.py
+++ filter_out_reads.py
@@ -2,7 +2,6 @@
 import sys
 import ParseFastQ
 import argparse
-import itertools
 
 def filterReads(inFilename, outFilename, ns):
     parser = ParseFastQ.ParseFastQ(inFilename)  # optional arg: headerSymbols allows changing the header symbols
@@ -15,36 +14,12 @@ def filterReads(inFilename, outFilename, ns):
         output.write("%s\n%s\n%s\n%s\n" % (record[0], record[1], record[2], record[3]))
         remained += 1
     print "total #reads: %d" % total
-    print "remaining #reads: %d" % remained
-
-def filterPEReads(inFilename1, inFilename2, outFilename1, outFilename2, ns):
-    pa1 = ParseFastQ.ParseFastQ(inFilename1)
-    pa2 = ParseFastQ.ParseFastQ(inFilename2)
-    out1 = open(outFilename1, 'w')
-    out2 = open(outFilename2, 'w')
-    total = remained = 0
-    for r1, r2 in itertools.izip(pa1, pa2):
-        total += 1
-        if 'N'*ns in r1[1] or 'N'*ns in r2[1]:
-            continue
-        out1.write("%s\n%s\n%s\n%s\n" % (r1[0], r1[1], r1[2], r1[3]))
-        out2.write("%s\n%s\n%s\n%s\n" % (r2[0], r2[1], r2[2], r2[3]))
-        remained += 1
-    print "total #reads: %d" % total
-    print "remaining #reads: %d" % remained
-    
+    print "remaining #reads: %d" % remained    
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Reads filter')
-    group = parser.add_mutually_exclusive_group(required=True)
-    subgroup = group.add_argument_group()
-    subgroup.add_argument('-1', '--read1')
-    subgroup.add_argument('-2', '--read2')
-    group.add_argument('-u', '--read')
-    parser.add_argument('-o', '--output', help='prefix of output files', required=True)
+    parser = argparse.ArgumentParser(description='A tool that filters out single-end reads containing at least a specified number of Ns')
+    parser.add_argument('input')
+    parser.add_argument('output')
     parser.add_argument('-n','--number', type=int, default=1, help='number of N')
     args = parser.parse_args()
-    if args.read:
-        filterReads(args.read, "%s.fq" % args.output, args.number)
-    else:
-        filterPEReads(args.read1, args.read2, "%s1.fq" % args.output, "%s2.fq" % args.output, args.number)
+    filterReads(args.input, args.output, args.number)
